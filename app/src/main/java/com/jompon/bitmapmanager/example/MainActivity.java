4d0cc546b6adc367aced2f0e22a661da9816acb0
jompons <snookerace6211@gmail.com>
2018-02-23 18:51:33 +0700

    Add save button

--- app/src/main/java/com/jompon/bitmapmanager/example/MainActivity.java
+++ app/src/main/java/com/jompon/bitmapmanager/example/MainActivity.java
@@ -17,6 +17,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -61,7 +63,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if( mPhotoURI == null && data.getExtras() != null )
                     mPhotoURI = data.getData();
 
-                save(mPhotoURI);
                 imgPhoto.setImageURI(mPhotoURI);
             }
             if (requestCode == Constant.REQUEST_SELECT_PICTURE) {
@@ -75,7 +76,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         bitmapManager.copyFile(new File(bitmapManager.getRealPathFromUri(mPhotoURI)), sdImageMainDirectory);
                         mPhotoURI = Uri.fromFile(sdImageMainDirectory);   //reference to new image path
 
-                        save(mPhotoURI);
                         imgPhoto.setImageURI(mPhotoURI);
                     }catch (Exception e){
                         if( ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ){
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 }
             }
+            invalidateOptionsMenu();
         }
     }
 
@@ -163,6 +164,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private void reset( )
+    {
+        mPhotoURI = null;
+        rotate = 0;
+        imgPhoto.setImageResource(R.mipmap.ic_launcher);
+        imgPhoto.setRotation(rotate);
+        invalidateOptionsMenu();
+    }
+
     private void showPermissionAlertDialog(String message)
     {
         AlertDialog.Builder abPermission = new AlertDialog.Builder(this);
@@ -182,5 +192,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
         AlertDialog dialog = abPermission.create();
         dialog.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        MenuItem menuSave = menu.findItem(R.id.action_save);
+        if( mPhotoURI == null )     menuSave.setVisible(false);
+        else                        menuSave.setVisible(true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if( item.getItemId() == R.id.action_save ){
+
+            save(mPhotoURI);
+            reset();
+            Toast.makeText(getApplicationContext(), R.string.alert_saved, Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
